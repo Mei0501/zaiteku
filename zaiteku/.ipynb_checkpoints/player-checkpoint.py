48b4cf01816abfb78a8759b54d043b95bb37ce2b
Mei0501 <shinohara.mei.tdr@gmail.com>
2022-04-30 11:19:52 +0900

    Tird commit.

--- zaiteku/.ipynb_checkpoints/player-checkpoint.py
+++ zaiteku/.ipynb_checkpoints/player-checkpoint.py
@@ -18,12 +18,12 @@ class Player():
         if not self.is_finished:
             self.position = self.position + role_dice()
             if self.position > 30:
-                self.position = 0 
+                self.position -= 0 
                 self.money = self.money + 1000
 
                 if self.money >= 10000:
                     self.is_finished = True
-            self._get_event()
+            _get_event()
         else:
              raise RuntimeError("already finished")
 
@@ -42,16 +42,20 @@ class Player():
         else: #position in news_positions:
             self._draw_postcard()
     
-    def _draw_zaitekucard(self):
+    def _draw_zaitekucard():
         print("ziteku")
     
-    def _draw_newscard(self):
+    def _draw_newscard():
             print("news")
     
-    def _draw_postcard(self):
+    def _draw_postcard():
             print("post")
     
     
+#get_event(3)
+
+    def _event(self):
+        print("hoge")
 
 #サイコロを２個ふった数をかえす
 def role_dice():
