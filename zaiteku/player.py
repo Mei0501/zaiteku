59b5882de5ed263092f28b565be3b77c23d34ef8
Mei0501 <shinohara.mei.tdr@gmail.com>
2022-04-30 10:49:59 +0900

    Second commit.

--- zaiteku/player.py
+++ zaiteku/player.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 
 import random
@@ -22,9 +23,36 @@ class Player():
 
                 if self.money >= 10000:
                     self.is_finished = True
-
+            _get_event()
         else:
              raise RuntimeError("already finished")
+
+    def _get_event(self):
+        zaiteku_positions = [2, 5, 11, 16, 19, 23, 31, 34, 38, 46, 55]
+        news_positions = [3, 7, 12, 21, 28, 32, 39, 44, 56]
+        post_positions =  [4,13,18,26,36,40,47,53]
+    
+    
+        if self.position in zaiteku_positions:
+            self._draw_zaitekucard()
+    
+        elif self.position in news_positions:
+            self._draw_newscard()
+    
+        else: #position in news_positions:
+            self._draw_postcard()
+    
+    def _draw_zaitekucard():
+        print("ziteku")
+    
+    def _draw_newscard():
+            print("news")
+    
+    def _draw_postcard():
+            print("post")
+    
+    
+#get_event(3)
 
     def _event(self):
         print("hoge")
