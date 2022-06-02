9ba313c64921dac99a09bdbc3347f12f4cdc58fd
Mei0501 <shinohara.mei.tdr@gmail.com>
2022-06-02 14:44:44 +0900

    trivial change

--- zaiteku/event.py
+++ zaiteku/event.py
@@ -2,6 +2,7 @@
 
 import sys
 from dice import role_dice
+import random
 
 sys.path.append('/Users/mizo/Project/zaiteku/zaiteku')
 
@@ -155,14 +156,14 @@ def _exit_special_bonus(player):
 def _last_special_bonus(player):
     player.money += 800
 
-#幸運のサイコロゲーム
+
 def _lucky_chance(player):
     player.position = 30
-    bet_money = input("Please enter the bet 100-500 >>>")
+    bet_money = int(input("Please enter the bet 100-500 >>>"))
     player.money -= bet_money
     for _ in range(3):
-        dice_eyes3 = random.randint(1,6)
-        dice_eyes4 = random.randint(1,6)
+        dice_eyes3 = random.randint(1, 6)
+        dice_eyes4 = random.randint(1, 6)
         if dice_eyes3 == dice_eyes4:
             player.money += bet_money * 10
         break
